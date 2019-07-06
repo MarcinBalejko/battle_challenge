37e8b7c19d5c0cf1a014d21da2961b2c39f45c2d
MarcinBalejko <mbbm9@o2.pl>
2019-07-07 00:01:33 +0200

    :white_check_mark: Entering names

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-    get "/" do
-        'Testing infrastructure working!'
-end
+    get '/' do
+        erb :index
+    end
+
+    post '/names' do
+        @player_1_name = params[:player_1_name]
+        @player_2_name = params[:player_2_name]
+        erb :play
+    end
+
     run! if app_file == $0
 end
