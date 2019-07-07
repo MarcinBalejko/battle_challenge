76d4c53cd24c357a2b472ed7836230f8eedeee6b
MarcinBalejko <mbbm9@o2.pl>
2019-07-07 21:32:32 +0200

    :white_check_mark: web_helpers

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
     scenario 'see Player 2 hit points' do
-        visit('/')
-        fill_in :player_1_name, with: 'Dave'
-        fill_in :player_2_name, with: 'Mittens'
-        click_button 'Submit'
+        sign_in_and_play
         expect(page).to have_content 'Mittens: 60HP'
     end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
 Capybara.app = Battle
+require 'features/web_helpers.rb'
 
 require 'simplecov'
 SimpleCov.start
