72b316290961635c8cfe5f90279878dd47618a8f
MarcinBalejko <mbbm9@o2.pl>
2019-07-07 15:55:50 +0200

    :construction: sessions

--- app.rb
+++ app.rb
@@ -1,13 +1,15 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+    enable :sessions
+
     get '/' do
         erb :index
     end
 
     post '/names' do
-        @player_1_name = params[:player_1_name]
-        @player_2_name = params[:player_2_name]
+        session[:player_1_name] = params[:player_1_name]
+        session[:player_2_name] = params[:player_2_name]
         erb :play
     end
 
