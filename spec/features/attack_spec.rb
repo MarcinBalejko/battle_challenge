9522db3109bbda79b92faef68815c352c9b9cc69
MarcinBalejko <mbbm9@o2.pl>
2019-07-09 00:18:17 +0200

    :recycle: Refactored

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -6,8 +6,7 @@ feature 'Attacking' do
     end
     scenario 'reduce Player 2 HP by 10' do
         sign_in_and_play
-        click_link 'Attack'   #?????
-            #??????   OD TAD ZACZAC 
+        click_link 'Attack' 
         expect(page).not_to have_content 'Mittens: 60HP'
         expect(page).to have_content 'Mittens: 50HP'
       end
