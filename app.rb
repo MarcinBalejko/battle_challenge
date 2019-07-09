7094e795dda7564323381eba397196f254eff932
MarcinBalejko <mbbm9@o2.pl>
2019-07-09 17:41:15 +0200

    :bug: Bug detected( HP reduction)

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
+require './lib/game'
 require './lib/player'
+
 class Battle < Sinatra::Base
     enable :sessions
 
@@ -8,21 +10,23 @@ class Battle < Sinatra::Base
     end
 
     post '/names' do
-        $player_1 = Player.new(params[:player_1_name])
-        $player_2 = Player.new(params[:player_2_name])
+
+        player_1 = Player.new(params[:player_1_name])
+        player_2 = Player.new(params[:player_2_name])
+        $game = Game.new(player_1, player_2)
         redirect '/play'
       end
       
       get '/play' do
-        @player_1_name = $player_1.name
-        @player_2_name = $player_2.name
+
+        @game = $game
         erb :play
       end
       
       get '/attack' do
-        @player_1 = $player_1
-        @player_2 = $player_2
-        Game.new.attack(@player_2)
+
+        @game = $game
+        @game.attack(@game.player_2)
         erb :attack
       end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,20 @@
 require './lib/game.rb'
 describe Game do
-    subject(:game) { described_class.new }
+    subject(:game) { described_class.new(player_1, player_2) }
     let(:player_1) { double :player }
     let(:player_2) { double :player }
+
+    describe '#player_1' do
+        it 'retrieves the first player' do
+            expect(game.player_1).to eq player_1
+        end
+    end
+
+    describe '#player_2' do
+        it 'retrieves the second player' do
+            expect(game.player_2).to eq player_2
+        end
+    end
 
     describe '#attack' do
         it 'damages the player' do
