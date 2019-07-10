68fcd376b2cc2b11b6c29518e39c3e573506d8d9
MarcinBalejko <mbbm9@o2.pl>
2019-07-10 13:51:31 +0200

    :bug: :white_check_mark: Bug fixed

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,13 +1,20 @@
 feature 'Attacking' do
-    scenario 'attack Player 2' do
-        sign_in_and_play
-        click_link 'Attack'
-        expect(page).to have_content 'Dave attacked Mittens'
+    context 'After clicking Attack link' do
+        before do
+            sign_in_and_play
+            click_link 'Attack'
+        end
+        scenario 'attack Player 2' do
+            sign_in_and_play
+            click_link 'Attack'
+            expect(page).to have_content 'Dave attacked Mittens'
+        end
+        scenario 'reduce Player 2 HP by 10' do
+            sign_in_and_play
+            click_link 'Attack'
+            click_link 'OK' 
+            expect(page).not_to have_content 'Mittens: 60HP'
+            expect(page).to have_content 'Mittens: 50HP'
+        end
     end
-    scenario 'reduce Player 2 HP by 10' do
-        sign_in_and_play
-        click_link 'Attack' 
-        expect(page).not_to have_content 'Mittens: 60HP'
-        expect(page).to have_content 'Mittens: 50HP'
-      end
 end
