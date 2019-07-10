bb12893bd8bad6388f2dc926dfba28ad56ab0d72
MarcinBalejko <mbbm9@o2.pl>
2019-07-10 16:40:36 +0200

    :recycle: Refactored

--- app.rb
+++ app.rb
@@ -10,7 +10,6 @@ class Battle < Sinatra::Base
     end
 
     post '/names' do
-
         player_1 = Player.new(params[:player_1_name])
         player_2 = Player.new(params[:player_2_name])
         $game = Game.new(player_1, player_2)
@@ -18,13 +17,11 @@ class Battle < Sinatra::Base
       end
       
       get '/play' do
-
         @game = $game
         erb :play
       end
       
       get '/attack' do
-
         @game = $game
         @game.attack(@game.player_2)
         erb :attack
