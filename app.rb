77f64d0a9daf8a513aad0d82602b8399a22585d2
MarcinBalejko <mbbm9@o2.pl>
2019-07-11 22:29:53 +0200

    :recycle: Refactored

--- app.rb
+++ app.rb
@@ -24,8 +24,13 @@ class Battle < Sinatra::Base
       get '/attack' do
         @game = $game
         @game.attack(@game.player_2)
-        @game.switch_turns
+        
         erb :attack
+      end
+
+      post '/switch-turns' do
+        $game.switch_turns
+        redirect('/play')
       end
 
     run! if app_file == $0
