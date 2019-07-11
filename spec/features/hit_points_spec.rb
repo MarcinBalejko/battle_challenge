8e1ae33c5f79078aaf74e452c7fd006f709ffa22
MarcinBalejko <mbbm9@o2.pl>
2019-07-11 17:06:50 +0200

    :art: Refactored

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -5,7 +5,7 @@ feature 'View hit points' do
     end
 
     scenario 'see Player 1 hit points' do
-        sign_in_and_play  ##########
+        sign_in_and_play
         expect(page).to have_content 'Dave: 60HP'
     end
 end
