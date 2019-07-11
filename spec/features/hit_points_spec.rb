5008872ab9494bd3133cbb0bb314b8e2b4437d31
MarcinBalejko <mbbm9@o2.pl>
2019-07-11 17:04:08 +0200

    :white_check_mark: Player 1 hit points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,4 +3,9 @@ feature 'View hit points' do
         sign_in_and_play
         expect(page).to have_content 'Mittens: 60HP'
     end
+
+    scenario 'see Player 1 hit points' do
+        sign_in_and_play  ##########
+        expect(page).to have_content 'Dave: 60HP'
+    end
 end
