16af45863b7853018a99e517991b98f90c1237d0
MarcinBalejko <mbbm9@o2.pl>
2019-07-11 23:33:48 +0200

    :recycle: Refactored

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -2,18 +2,18 @@ feature 'Attacking' do
     context 'After clicking Attack link' do
         before do
             sign_in_and_play
-            click_link 'Attack'
+            click_button 'Attack'
         end
 
         scenario 'attack second player' do
             sign_in_and_play
-            click_link 'Attack'
+            click_button 'Attack'
             expect(page).to have_content 'Dave attacked Mittens'
         end
 
         scenario 'reduce second player HP by 10' do
             sign_in_and_play
-            click_link 'Attack'
+            click_button 'Attack'
             click_button 'OK' 
             expect(page).not_to have_content 'Mittens: 60HP'
             expect(page).to have_content 'Mittens: 50HP'
@@ -21,11 +21,11 @@ feature 'Attacking' do
 
         scenario 'be attacked by Player 2' do
             sign_in_and_play
-            click_link 'Attack'
+            click_button 'Attack'
             click_button 'OK'
-            click_link 'Attack'
+            click_button 'Attack'
             expect(page).to have_content 'Mittens attacked Dave'
         end
     end
-    
+
 end
--- spec/features/switch_turns_spec.rb
+++ spec/features/switch_turns_spec.rb
@@ -8,7 +8,7 @@ feature 'Switching turns' do
 
             scenario 'after player 1 attacks' do
                 sign_in_and_play
-                click_link 'Attack'
+                click_button 'Attack'
                 click_button 'OK'
                 expect(page).not_to have_content "Dave's turn"
                 expect(page).to have_content "Mittens's turn"
