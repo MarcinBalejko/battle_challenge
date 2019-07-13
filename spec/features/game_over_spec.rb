10d6d61025ef814038268ae2cb84b9ac9025d3d1
MarcinBalejko <mbbm9@o2.pl>
2019-07-13 22:33:09 +0200

    :construction: game over feature spec

--- spec/features/game_over_spec.rb
+++ spec/features/game_over_spec.rb
@@ -1,14 +1,15 @@
 feature 'Game over' do
-    context 'when Player 1 reaches 0 HP first' do
-        before do
-            sign_in_and_play
-            attack_and_confirm
-            allow(Kernel).to receive(:rand).and_return(60)
-        end
-
-        scenario 'Player 1 loses' do
-            click_button 'Attack'
-            expect(page).to have_content 'Dave loses!'
-        end
+  context 'when Player 1 reaches 0 HP first' do
+    before do
+      sign_in_and_play
+      attack_and_confirm
+      allow(Kernel).to receive(:rand).and_return(60)
     end
-end
+
+    scenario 'Player 1 loses' do
+        click_button 'Attack'
+        expect(page).to have_content 'Dave loses!'
+    end
+  end
+end 
+  
